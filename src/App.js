8b921c318bc47ccc0e61846665d65b9213235a60
Be-libra <50490129+Be-libra@users.noreply.github.com>
2021-06-11 13:31:02 +0530

    initial setup files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <center><h3>this  is webpack boilerplate for reactjs</h3></center>
+    )
+  }
+}
+
+export default App
